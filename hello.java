54f240eca9e3704db5a28f79e7e73cd24404dccb
dev2 <dev1@gmail.com>
2023-03-15 19:18:43 +0530

    even lines

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class hello ()    #from dev1 chnage1 
-{
+{ #from dev1
 	public static void main()  #from dev1 chnage1
-	{
+	{  #from dev1
 		system.out.println("hello from dev1 change1");
-	}
+	}  #from dev1
 }
